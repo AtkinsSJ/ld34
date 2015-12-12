bc5ae336d390b8c0d95fa34d9f362da58929d2c7
Samuel Atkins <sam@samatkins.co.uk>
2015-12-12 10:21:42 +0000

    First commit

---
+++ desktop/src/uk/co/samatkins/ecosystem/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.co.samatkins.ecosystem.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.co.samatkins.ecosystem.EcosystemGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EcosystemGame(), config);
+	}
+}
---
+++ html/src/uk/co/samatkins/ecosystem/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package uk.co.samatkins.ecosystem.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import uk.co.samatkins.ecosystem.EcosystemGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new EcosystemGame();
+        }
+}
