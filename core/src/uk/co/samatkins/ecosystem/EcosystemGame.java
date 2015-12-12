41d0ddb43fa0dcae3fa8d9acdabcf2fce6b1ef0d
Samuel Atkins <sam@samatkins.co.uk>
2015-12-12 12:13:47 +0000

    Evaporation!

--- core/src/uk/co/samatkins/ecosystem/EcosystemGame.java
+++ core/src/uk/co/samatkins/ecosystem/EcosystemGame.java
@@ -1,6 +1,7 @@
 package uk.co.samatkins.ecosystem;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -20,13 +21,18 @@ public class EcosystemGame extends ApplicationAdapter {
 	}
 
 	class Tile {
-		Terrain terrain;
+		Terrain terrain = Terrain.Air;
+		float humidity = 0;
 	}
 
 	SpriteBatch batch;
 
 	int worldWidth, worldHeight;
 	Tile[][] tiles;
+
+	final Color colNoHumidity = new Color(1,1,1,1),
+				colMaxHumidity = new Color(0,0,1,1);
+	final Color dumpColor = new Color();
 
 	public static int randomInt(Random random, int minInclusive, int maxExclusive) {
 		return minInclusive + random.nextInt(maxExclusive - minInclusive);
@@ -52,6 +58,7 @@ public class EcosystemGame extends ApplicationAdapter {
 				Tile tile = new Tile();
 				if (y < depth) {
 					tile.terrain = Terrain.Soil;
+					tile.humidity = random.nextFloat();
 				} else {
 					tile.terrain = Terrain.Air;
 				}
@@ -62,14 +69,40 @@ public class EcosystemGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+
+		// update humidity
+		for (int x = 0; x < worldWidth; x++) {
+			for (int y = 0; y < worldHeight; y++) {
+				Tile tile = tiles[x][y];
+				if (tile.terrain != Terrain.Air) { // TODO: Air humidity???
+					// Evaporation
+					if (y < (worldHeight-1)) {
+						Tile above = tiles[x][y + 1];
+						if (above.terrain == Terrain.Air) {
+							tile.humidity *= 0.999f;
+						}
+					}
+
+					// Osmosis
+
+				}
+			}
+		}
+
+
 		Gdx.gl.glClearColor((113f/255f), (149f/255f), (255f/255f), 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 
 		for (int x = 0; x < worldWidth; x++) {
 			for (int y = 0; y < worldHeight; y++) {
-				Texture texture = tiles[x][y].terrain.texture;
+				Tile tile = tiles[x][y];
+				Texture texture = tile.terrain.texture;
 				if (texture != null) {
+
+					// LibGDX is STUPID why does lerping a color edit the color??!?!??!?!?!?
+					dumpColor.set(colNoHumidity);
+					batch.setColor(dumpColor.lerp(colMaxHumidity, tile.humidity));
 					batch.draw(texture, x * 16, y * 16);
 				}
 			}
